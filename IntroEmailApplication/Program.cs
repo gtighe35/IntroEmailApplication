fe6534e0d32fdd396f2742ef26f95219d2d21a82
gtighe35 <gagetighe@gmail.com>
2023-08-27 19:15:11 -0500

    Final

--- IntroEmailApplication/Program.cs
+++ IntroEmailApplication/Program.cs
@@ -13,7 +13,7 @@ namespace IntroEmailApplication
       Console.WriteLine("Welcome to the command line email client!");
 
       var loop = true;
-  
+      // We loop here so that the user can send multiple emails
       while (loop)
       {
         Console.WriteLine();
@@ -22,7 +22,9 @@ namespace IntroEmailApplication
         var mail = new MimeMessage();
 
         mail.From.Add(new MailboxAddress("Gage Tighe", "gtgtighe35@gmail.com")); // TODO: update to your own name and email address
-        try
+        //This part is for taking in the information of the email from the user
+
+        try //If there is a " " in the Console.ReadLine(), this will result in an exception
         {
           Console.Write("To: ");
           mail.To.Add(new MailboxAddress("", Console.ReadLine()));
@@ -31,7 +33,7 @@ namespace IntroEmailApplication
         {
            Console.WriteLine("An error resulted from entering the email: ");
            Console.WriteLine(ex.Message);
-           continue;
+           continue; //This will go to the next loop
         }
        
 
@@ -41,6 +43,8 @@ namespace IntroEmailApplication
         Console.Write("Body: ");
         mail.Body = new TextPart("plain") { Text = Console.ReadLine() };
 
+//After having all the info, we will try to send the message
+  try{
         using (var client = new SmtpClient())
         {
           client.Connect("smtp.gmail.com", SmtpPort, IsSslConnection);
@@ -50,10 +54,15 @@ namespace IntroEmailApplication
 
           Console.WriteLine("Message sent succesfully!");
         }
-
+  }
+  catch(Exception ex){
+    Console.WriteLine("Error occurred while attempting to send email");
+    Console.WriteLine(ex.Message);
+    // I could have logic to make this go to the send another email prompt, but I am fine with the way it handles this exception
+  }
         Console.WriteLine();
         Console.Write("Would you like to send another email? (Y/N): ");
-
+        //This will instantly terminate if they press any other key
         if (Console.ReadKey().Key != ConsoleKey.Y)
         {
           loop = false;
